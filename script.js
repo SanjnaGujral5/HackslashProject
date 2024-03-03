900b3300fa8f4af398c20c79933abd05db915a07
SanjnaGujral5 <sanjnagujral01@gmail.com>
2024-03-03 13:23:14 +0530

    Hackslash

---
+++ script.js
@@ -0,0 +1,22 @@
+// modeBtn.addEventListener("click", () => {
+//     if (currMode === "Light"){
+//         currMode = "dark";
+//         body.classList.add("dark");
+//         body.classList.remove("light");
+
+
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+  }
+//   function init(){
+//     const photo = document.getElementById('photo');
+//     photo.onclick = function(){
+//         photo.style.transform = 'rotate(180 degree)';
+//     }
+// }
+    
+    
+    
+  
